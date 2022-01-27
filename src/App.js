8e1638fa46b1035c4cfb526a22f3b21f3e6d1f1e
Alex Teerlinck <shadows32426@gmail.com>
2022-01-27 17:40:28 -0600

    module 3 (and maybe 2. idk)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import Word from './components/Word';
+import Counter from './components/Counter';
 import './App.css';
-import Word from './components/Word'
 
 class App extends Component {
+  state = {
+    words: [
+      { id: 1, word: 'banana', color: 'yellow' },
+      { id: 2, word: 'apple', color: 'red' },
+      { id: 3, word: 'lime', color: 'green' },
+    ]
+  }
+  handleDelete = (wordId) => {
+    const words = this.state.words.filter(w => w.id !== wordId);
+    this.setState({ words:words });
+  }
+  componentDidMount() {
+    console.log("App mounted");
+  }
   render() { 
+    const { words } = this.state;
     return ( 
       <div className="App">
         <header className="App-header">
           nugatory
         </header>
-        <Word />
+        { words.map(word => 
+          <Word 
+            key={ word.id } 
+            word={ word } 
+            onDelete={ this.handleDelete } />
+        )}
+        <Counter totalWords={ words.length } />
       </div>
      );
   }
 }
-
+ 
 export default App;
--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 
 class Word extends Component {
-  state = {
-    word: 'banana',
-    color: 'yellow',
-  }
+  wordEl = React.createRef();
   // helper method
   renderColor(color) {
     return (color === undefined || color === null ? 'black' : color);
   }
-  handleClick = () =>{
-    // convert the function to an arrow function
-    // arrow functions inherit 'this' keyword
-    console.log(this.state.color);
-    // the setState method is inherited from the base Component class
-    // when a component's state is altered, it is re-rendered asynchronously by react
-    this.setState({ color: 'black' })
+  // return random number between min & max
+  getRandomNumber = (min, max) => Math.random() * (max - min) + min;
+  // return random number between 0 and window height
+  getRandomTop = (height) => parseInt(this.getRandomNumber(0, window.innerHeight - height));
+  // return random number between 0 and window width
+  getRandomLeft = (width) => parseInt(this.getRandomNumber(0, window.innerWidth - width));
+
+  componentDidMount() {
+    const el = this.wordEl.current;
+    el.style.top = this.getRandomTop(el.clientHeight) + 'px';
+    el.style.left = this.getRandomLeft(el.clientWidth) + 'px';
+  }
+  componentWillUnmount() {
+    console.log(`goodbye, ${ this.props.word.word }`);
   }
   render() { 
+    const { onDelete, word } = this.props;
     return (
-      <div onClick={ this.handleClick } className='Word' style={{ color:this.renderColor(this.state.color) }}>
-        { this.state.word }
+      <div ref={ this.wordEl } id={ word.id } onClick={ () => onDelete(word.id) } className='Word' style={{ color:this.renderColor(word.color) }}>
+        { word.word }
       </div>
     );
   }
